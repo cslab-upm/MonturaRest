115aa0a0579907feead28feb880695ac3b314589
danielfdzperez <daniel.fdz.perez@gmail.com>
2018-02-13 16:49:54 +0100

    temperatura, comandos selectivos, hilo para publicacion de azimut

--- apiRest4/montura.py
+++ apiRest4/montura.py
@@ -53,9 +53,15 @@ class montura(monturaGloria):
             if (puerto == 'exit'):
                 print('Saliendo...')
                 exit()
-                
+
             self.serial.setPuerto(puerto)
         print('Puerto abierto: ' +self.serial.puerto)
+
+    def comunicarAzimut(self):
+        while True:
+	    print(self.getAz())
+            time.sleep(10)
+	
     # Al instanciar iniciamos abriendo el puerto serie
     def __init__(self):
         print('Iniciando...')
@@ -64,6 +70,8 @@ class montura(monturaGloria):
         self.getDec()
         self.getAz()
         self.getAlt()
+	thread = Thread(target = self.comunicarAzimut)
+	thread.start()
 
     def setCola(self, cola):
         self.cola = cola
@@ -97,28 +105,29 @@ class montura(monturaGloria):
     # Devuelve el valor de la altitud
     def getAlt(self):
         print('Devuelve la altitud')
-	mensaje = ':GA#'
-	# Devuelve sDD*MM:SS# pero detecta el * como simbolo desconocido
-	altitud = self.getPosicion(mensaje)
-	altitud = altitud[0:3]+":"+altitud[4:-1]
-	self.alt = altitud
-	return altitud
+        mensaje = ':GA#'
+        # Devuelve sDD*MM:SS# pero detecta el * como simbolo desconocido
+        altitud = self.getPosicion(mensaje)
+	print(altitud)
+        altitud = altitud[0:3]+":"+altitud[4:-1]
+        self.alt = altitud
+        return altitud
 
     # Devuelve el valor de azimut
     def getAz(self):
         print('Devuelve el azimut')
-	mensaje = ':GZ#'
-	# Devuelve DDD*MM:SS#
-	azimut = self.getPosicion(mensaje)
-	azimut = azimut[0:3]+":"+azimut[4:-1]
-	self.az = azimut
-	return azimut
+        mensaje = ':GZ#'
+        # Devuelve DDD*MM:SS#
+        azimut = self.getPosicion(mensaje)
+        azimut = azimut[0:3]+":"+azimut[4:-1]
+        self.az = azimut
+        return azimut
 
     # Obtiene la posicion en la que se encuentra el telescopio
     def getPosicion(self, mensaje):
         respuesta = self.serial.enviar(mensaje, respuesta = True)
         return respuesta
-        
+
 
 # ---------------------------------------------------------
 
@@ -130,16 +139,16 @@ class montura(monturaGloria):
 
         coordenadas0 = [0.0, 0.0]
         coordenadas1 = [0.0, 0.0]
-        
+
         while (self.seguimiento == True or self.llegando == True):
-            
+
             t2 = time.clock()
-            
+
             if (t2-t1 >= tEspera):
-                
+
                 comparar = False
                 llegandoModo = 'az-alt'
-                
+
                 if self.llegando == True:
                     comparar = True
                     #Evitamos bugs
@@ -147,11 +156,11 @@ class montura(monturaGloria):
                     if self.llegandoModo == 'az-alt':
                         coordenadas0[0] = self.toNumero(self.az)
                         coordenadas0[1] = self.toNumero(self.alt)
-                        
+
                     elif self.llegandoModo == 'ra-dec':
                         coordenadas0[0] = self.toNumero(self.ra)
                         coordenadas0[1] = self.toNumero(self.dec)
-                    
+
                 self.ra = self.getRA()
                 self.dec = self.getDec()
                 self.az = self.getAz()
@@ -167,7 +176,7 @@ class montura(monturaGloria):
                     if llegandoModo == 'az-alt':
                         coordenadas1[0] = self.toNumero(self.az)
                         coordenadas1[1] = self.toNumero(self.alt)
-                        
+
                     elif llegandoModo == 'ra-dec':
                         coordenadas1[0] = self.toNumero(self.ra)
                         coordenadas1[1] = self.toNumero(self.dec)
@@ -177,30 +186,30 @@ class montura(monturaGloria):
                         #########
                         self.llegando = False # Paramos
                         if self.haLlegado(coordenadas1, self.llegandoPosicion, 1.0) == True:
-                            print 'Ha llegado'
+                            print('Ha llegado')
                             self.resultadoLlegando = "Sin incidencias"
-                            
+
                         else:
-                            print 'No ha llegado'
+                            print('No ha llegado')
                             self.resultadoLlegando = "Error en el movimiento"
-                            print self.resultadoLlegando
-                        
+                            print(self.resultadoLlegando)
+
                 # Historial de posiciones  ###################
                 #self.posiciones.insert({'posicion': {'fecha': time.strftime("%d/%m/%y"), 'hora': time.strftime("%H:%M:%S"), 'coordenadas': {'ra': self.ra, 'dec': self.dec}}})
                 t1 = time.clock()
                 tEspera = self.tSeguimiento - (t1-t2) # intervalo de actualizacion - tiempo estimado que tardara
                 #print 'Tiempo de espera: {}'.format(tEspera) #################
-                
+
             time.sleep(0.1)
-                
+
         self.aceptarThread = True
         print('Seguimiento finalizado')
-       
+
 
     # Activa o desactiva el seguimiento
     def setSeguimiento(self,b, esperar=False):
         resultado = 'Nada ha cambiado'
-        
+
         if (self.seguimiento == False and (b == True or esperar == True)):
             if self.aceptarThread == True:
                 self.seguimiento = b
@@ -209,12 +218,12 @@ class montura(monturaGloria):
                 resultado = 'Seguimiento iniciado'
             else:
                 resultado = 'Antes de iniciar un nuevo seguimiento debe terminar el anterior'
-        
+
         elif self.seguimiento == True and b == False:
             self.seguimiento = b
             resultado = 'Deteniendo seguimiento'
         return resultado
-        # Podemos crear un thread aqui dentro
+    # Podemos crear un thread aqui dentro
 
     # Cambiamos el tiempo entre obtener posiciones en el seguimiento
     def setTasaSeguimiento(self, tiempo):
@@ -224,18 +233,18 @@ class montura(monturaGloria):
     # Ajustamos la velocidad de movimiento del telescopio
     def setSlewRate(self, n):
         print('Slew rate')
-        
+
     def cambiarPermitirMovimiento(self, b):
         self.permitirMovimiento = b
 
     def estaMoviendose(self, coordenadas0, coordenadas1, margen):
         print('Margen: ')
-        print margen
+        print(margen)
         # coordenadas 1 es la actual
         # coordenadas 0 es la anterior
         if (coordenadas0[0] >= coordenadas1[0] - margen) and (coordenadas1[0] + margen >= coordenadas0[0] ) and (coordenadas0[1] >= coordenadas1[1] - margen) and (coordenadas1[1] + margen >= coordenadas0[1]):
-                return False
- 
+            return False
+
         return True
 
     def toNumero(self, coordenada):
@@ -258,20 +267,20 @@ class montura(monturaGloria):
     def haLlegado(self, coordenadas1, coordenadas3, margen):
         # coordenadas 1 es la actual
         # coordenadas 3 es la objetivo
-        print 'C10: '
-        print coordenadas1[0]
+        print ('C10: ')
+        print (coordenadas1[0])
 
-        print 'C11: '
-        print coordenadas1[1]
+        print ('C11: ')
+        print (coordenadas1[1])
 
-        print 'C30: '
-        print coordenadas3[0]
+        print ('C30: ')
+        print (coordenadas3[0])
 
-        print 'C31: '
-        print coordenadas3[1]
-        
+        print ('C31: ')
+        print (coordenadas3[1])
+
         if (coordenadas1[0] >= coordenadas3[0] - margen) and (coordenadas3[0] + margen >= coordenadas1[0]) and (coordenadas1[1] >= coordenadas3[1] - margen) and (coordenadas3[1] + margen >= coordenadas1[1] ):
-                return True
+            return True
         return False
 
     def setLlegandoModo(self, modo, posicion):
@@ -290,11 +299,11 @@ class montura(monturaGloria):
             time.sleep(1)
         print (self.resultadoLlegando)
         setResultado(self.resultadoLlegando, iden)
-        
-        
-        
+
+
+
 # ------------------------------------------------------------------------
-    
+
 
     # Mueve el telescopio en una direccion durante unos milisegundos
     def moverPorTiempo(self, direccion, tiempo = 500):
@@ -302,24 +311,24 @@ class montura(monturaGloria):
         if not self.permitirMovimiento:
             resultado = 'Movimiento no disponible'
             return resultado
-        
+
         resultado = 'Sin incidencias'
-        
+
         # ----- Para el telescopio del laboratorio ------
         # mensaje = ":Mg%s%s#" % (direccion, tiempo)
 
-	# ----- Para el telescopio del observatorio ------
-	mensajeIniciar = ':M{}#'.format(direccion)
-	mensajeParar = ':Q{}#'.format(direccion)
+        # ----- Para el telescopio del observatorio ------
+        mensajeIniciar = ':M{}#'.format(direccion)
+        mensajeParar = ':Q{}#'.format(direccion)
 
-	# Comienza el movimiento
-	self.serial.enviar(mensajeIniciar) # No devuelve nada
-	#print('Mensaje {} enviado').format(mensajeIniciar) #########
-	# Paramos el movimiento tras un tiempo
-	th = Timer(tiempo/1000.0, self.serial.enviar, [mensajeParar])
-	th.start()
-	# Esperamos a que envie este mensaje para continuar
-	th.join()
+        # Comienza el movimiento
+        self.serial.enviar(mensajeIniciar) # No devuelve nada
+        #print('Mensaje {} enviado').format(mensajeIniciar) #########
+        # Paramos el movimiento tras un tiempo
+        th = Timer(tiempo/1000.0, self.serial.enviar, [mensajeParar])
+        th.start()
+        # Esperamos a que envie este mensaje para continuar
+        th.join()
         #print('Mensaje {} enviado').format(mensajeParar) ########
         return resultado
 
@@ -330,59 +339,59 @@ class montura(monturaGloria):
         if not self.permitirMovimiento:
             resultado = 'Movimiento no disponible'
             return resultado
-        
+
         resultado = 'Sin incidencias'
 
         if tipo == 0: # az-alt
-            
-	    g, m, s = coordenadas[1].split(':') #Grados, minutos, segundos
-	    mensaje = ":Sa{}*{}'{}#".format(g,m,s) # +00*00'00
-	    # Establecemos las coordenadas altitud del objetivo
-	    respuesta = self.serial.enviar(mensaje, respuesta = True)
-            
-	    if respuesta != '1': # La montura devuelve un error
-                return 'Error. No se ha podido establecer las coordenadas de altitud'
-			
-	    # Coordenadas de altitud aceptadas. Continuamos con azimut	    
-	    g, m = coordenadas[0].split(':') # Grados, minutos
-	    mensaje = ":Sz{}*{}#".format(g,m) # 000*00
+
+            g, m, s = coordenadas[1].split(':') #Grados, minutos, segundos
+            mensaje = ":Sa{}*{}'{}#".format(g,m,s) # +00*00'00
+            # Establecemos las coordenadas altitud del objetivo
             respuesta = self.serial.enviar(mensaje, respuesta = True)
 
-	    if respuesta != '1': # La montura devuelve un error
-		return 'Error. No se ha podido establecer las coordenadas de azimut'
+            if respuesta != '1': # La montura devuelve un error
+                return 'Error. No se ha podido establecer las coordenadas de altitud'
 
-	    # Coordenadas de azimut aceptadas. Solicitamos el movimiento a la montura
-	    mensaje = ':MA#'
-	    respuesta = self.serial.enviar(mensaje, respuesta = True)
-	    if respuesta != '0': # La montura devuelve dos posibles errores
+            # Coordenadas de altitud aceptadas. Continuamos con azimut	    
+            g, m = coordenadas[0].split(':') # Grados, minutos
+            mensaje = ":Sz{}*{}#".format(g,m) # 000*00
+            respuesta = self.serial.enviar(mensaje, respuesta = True)
+
+            if respuesta != '1': # La montura devuelve un error
+                return 'Error. No se ha podido establecer las coordenadas de azimut'
+
+            # Coordenadas de azimut aceptadas. Solicitamos el movimiento a la montura
+            mensaje = ':MA#'
+            respuesta = self.serial.enviar(mensaje, respuesta = True)
+            if respuesta != '0': # La montura devuelve dos posibles errores
                 return 'Error. Movimiento imposible. ' + respuesta[1:-1] # Filtramos el caracter que identifica el error
 
 
         elif tipo == 1: # RA-dec
             h, m = coordenadas[0].split(':') # Horas, minutos
-	    mensaje = ":Sr{}*{}#".format(h,m) # 00*00
-	    # Establecemos las coordenadas de ascension recta del objetivo
-	    respuesta = self.serial.enviar(mensaje, respuesta = True)
-		
-	    if respuesta != '1': # La montura devuelve un error
+            mensaje = ":Sr{}*{}#".format(h,m) # 00*00
+            # Establecemos las coordenadas de ascension recta del objetivo
+            respuesta = self.serial.enviar(mensaje, respuesta = True)
+
+            if respuesta != '1': # La montura devuelve un error
                 return 'Error. No se ha podido establecer las coordenadas de ascension recta'
 
             # Coordenadas de ascension recta aceptadas. Continuamos con declinacion
             g, m = coordenadas[1].split(':') # Grados, minutos
-	    mensaje = ":Sd{}*{}#".format(g,m) # +00*00
-	    print mensaje
-	    respuesta = self.serial.enviar(mensaje, respuesta = True)
+            mensaje = ":Sd{}*{}#".format(g,m) # +00*00
+            print mensaje
+            respuesta = self.serial.enviar(mensaje, respuesta = True)
 
-	    if respuesta != '1': # La montura devuelve un error
-		return 'Error. No se ha podido establecer las coordenadas de declinacion'
+            if respuesta != '1': # La montura devuelve un error
+                return 'Error. No se ha podido establecer las coordenadas de declinacion'
 
-	    # Coordenadas de declinacion aceptadas. Solicitamos el movimiento		
-	    mensaje = ':MS#'
+            # Coordenadas de declinacion aceptadas. Solicitamos el movimiento		
+            mensaje = ':MS#'
             respuesta = self.serial.enviar(mensaje, respuesta = True)	
 
-	    if respuesta != '0': # La montura devuelve dos posibles errores
+            if respuesta != '0': # La montura devuelve dos posibles errores
                 return 'Error. Movimiento imposible. ' + respuesta[1:-1]
-            
+
         # Todo ha funcionado sin incidencias
         return resultado
 
@@ -415,11 +424,27 @@ class montura(monturaGloria):
         self.serial.enviar(mensaje)            
     def moverOeste(self):
         mensaje = ':Mw#'
-        self.serial.enviar(mensaje)            
+        self.serial.enviar(mensaje)
+
+    #Obtener la temperatura
+    def temperatura(self):
+	mensaje = ":fT#"
+        respuesta = self.serial.enviar(mensaje, respuesta = True)
+	print(respuesta)
+
+    #Escribir cualquier comando que espere una respuesta y la imprime
+    def comandoRespuesta(self,mensaje):
+        respuesta = self.serial.enviar(mensaje, respuesta = True)
+	print(respuesta)
+
+    #Escribir un comando que no espera respuesta
+    def comando(self,mensaje):
+        self.serial.enviar(mensaje, respuesta = False)
+
 
 
     # Emplear?    
-    def detenerMovimiento(self, noBloquear = False):
+    def stop(self, noBloquear = False):
         self.permitirMovimiento = noBloquear
         mensaje = ':Q#'
         self.serial.enviar(mensaje)            
